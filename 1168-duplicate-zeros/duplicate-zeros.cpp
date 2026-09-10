2c1354283b3983889daf31a741469ddc2616caf6
agrawalannie3963-max <agrawalannie3963@gmail.com>
2026-09-10 07:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (50.02%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -1,53 +1,91 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-//         int n=arr.size();
-//         int i=0;
-//         int count=1;
-//         while(i<n-1)
+
+// int n=arr.size();
+// int j;
+// int i=0;
+// while(i<n)
+// {
+//     if(arr[i]==0)
+//     {
+//         int idx=i;
+//         for(int j=n-1;j>idx;j--)
 //         {
-//             if(arr[i]==0)
-//             {
-//                 arr[i+1]=0;
-//                 i=i+2;
-//                 count++;
-//             }
-//             else
-//             {
-//             i++;
-//             count++;
-//             }
-//             if(count==n)
-//             {
-//                 break;
-//             }
+//             arr[j]=arr[j-1];
 //         }
+//         arr[idx]=0;
+//         i=i+2;
+//     }
+//     else
+//     {
+//         i++;
+//     }
+// }
+
 //     }
 // };
+// int n=arr.size();
+// int zero_count=0;
+// for(int i=0;i<n;i++)
+// {
+//     if(arr[i]==0)
+//     {
+//         zero_count++;
+//     }
+// }
+// int fp=n-1;
+// int tp=n-zero_count;
+// while((fp>=tp)&&(fp!=0)&&(tp!=0))
+// {
+//     if(arr[tp]!=0)
+//     {
+//         arr[fp]=arr[tp];
+//     }
+//     else
+//     {
+//         arr[fp]=0;
+//         fp--;
+//         arr[fp]=0;
+//     }
+//     fp--;
+//     tp--;
+// }
+//     }
+// };
+
 int n=arr.size();
-int j;
-int i=0;
-while(i<n)
+int zeroCount=0;
+for(int i=0;i<n;i++)
 {
     if(arr[i]==0)
     {
-        int idx=i;
-        for(int j=n-1;j>idx;j--)
-        {
-            arr[j]=arr[j-1];
-        }
-        arr[idx]=0;
-        i=i+2;
-    }
-    else
-    {
-        i++;
+        zeroCount++;
     }
 }
-// for(int j=0;j<n;j++)
-// {
-//     arr.push_back(arr[j]);
-// }
+int tp=n-1;
+int fp;
+while(tp>=0 && zeroCount!=0)
+{
+    if(arr[tp]==0)
+    {
+        zeroCount--;
     }
+        fp=tp+zeroCount;
+    if(fp<n)
+    {
+        arr[fp]=arr[tp];
+    }
+    if(arr[tp]==0)
+    {
+        if((fp+1)<n)
+        {
+            arr[fp+1]=0;
+        }
+        
+    }
+    tp--;
+}
+}
 };
 
