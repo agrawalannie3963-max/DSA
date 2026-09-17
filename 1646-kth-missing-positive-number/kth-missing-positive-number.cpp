4e61a8ce064302e41333015ad2d756f06e9c06b6
agrawalannie3963-max <agrawalannie3963@gmail.com>
2026-09-18 00:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (61.08%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        // int n=arr.size();
-        // int miss=1;
-        // int i=0;
-        // while(i<n)
-        // {
-        //     if(arr[i]!=miss)
-        //     {
-        //         miss++;
-        //         k--;
-        //         if(k==0)
-        //         {
-        //             return miss;
-        //         }
-        //     }
-        //     else
-        //     {
-        //         miss++;
-        //         i++;
-        //     }
-        // }
-        // return k+arr[n-1];
-
         int n=arr.size();
-        int top=0;
-        int bottom=n-1;
-        int misselement;
-        while(top<=bottom)
+        int miss=1;
+        int i=0;
+        int copy=k;
+        while(i<n)
         {
-            int mid=(top+bottom)/2;
-            int misselement=arr[mid]-(mid+1);
-            if(misselement<k)
+            if(arr[i]!=miss)
             {
-                 top=mid+1;
+                k--;
+                if(k==0)
+                {
+                    return miss;
+                }
+                miss++;
             }
-            else if(misselement==k)
+            else
             {
-                bottom=mid-1;
-            }
-            else 
-            {
-                bottom=mid-1;
+                miss++;
+                i++;
             }
         }
-        return top+k;
-
-
-        }
+        return k+arr[n-1];
+    }
 };
+//         int n=arr.size();
+//         int top=0;
+//         int bottom=n-1;
+//         int misselement;
+//         while(top<=bottom)
+//         {
+//             int mid=(top+bottom)/2;
+//             int misselement=arr[mid]-(mid+1);
+//             if(misselement<k)
+//             {
+//                  top=mid+1;
+//             }
+//             else if(misselement==k)
+//             {
+//                 bottom=mid-1;
+//             }
+//             else 
+//             {
+//                 bottom=mid-1;
+//             }
+//         }
+//         return top+k;
+
+
+//         }
+// };
